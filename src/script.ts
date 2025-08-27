87541beb01f7b16825149ade4186d0212c566def
wakiullah <m.wakiullah1212@gmail.com>
2025-08-27 15:24:57 +0600

    learn about Dynamic type in typescript

--- src/script.ts
+++ src/script.ts
@@ -4,3 +4,19 @@ let b:number
 a = 'hello'
 b = 123
 console.log(a)
+
+const c:string[] =[]
+
+c.push('hello')
+// c.push(34)
+c.push('world')
+
+let d:{
+    name:string,
+    age:number
+}
+
+d = {
+    name:'hello',
+    age:34
+}
