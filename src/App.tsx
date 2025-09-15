e714d4b3b0e51087c0f70d41adcd55e840287284
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2025-09-15 15:06:03 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,8 @@ function App() {
     Warning = <InfoBox mode="hint" />;
   }
 
+  
+
   return (
     <div className="max-w-2xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-8">Create Note</h1>
