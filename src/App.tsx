f247c5984feadcd378458eb7451d9863277ac71c
wakiullah <m.wakiullah1212@gmail.com>
2025-09-17 10:42:09 +0600

    Edit button name

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
           type="submit"
           className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 text-lg font-medium"
         >
-          Create New Note
+          Create Note
         </button>
       </form>
 
